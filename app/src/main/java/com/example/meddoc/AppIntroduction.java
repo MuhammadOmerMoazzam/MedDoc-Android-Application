a0ed2678c14f59057412d71cd8e67ccca17426cb
MuhammadOmerMoazzam <153811108+MuhammadOmerMoazzam@users.noreply.github.com>
2024-06-23 00:45:16 +0500

    Removed and added some png files, changed the app logo.

--- app/src/main/java/com/example/meddoc/AppIntroduction.java
+++ app/src/main/java/com/example/meddoc/AppIntroduction.java
@@ -1,6 +1,9 @@
 package com.example.meddoc;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,23 @@ public class AppIntroduction extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_app_introduction);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Delay of 3 seconds (3000 milliseconds)
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start MainActivity
+                Intent intent = new Intent(AppIntroduction.this, MainActivity.class);
+                startActivity(intent);
+                // Finish the current activity
+                finish();
+            }
+        }, 3000); // 3000 milliseconds delay
     }
 }
